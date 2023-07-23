cf172336a4749ee057d1575ebf2ea66a6a5ce28b
misak <runnrunn400.rina@gmail.com>
2023-07-23 21:09:14 +0900

    paramsのtype追加

--- app/api/posts/[id]/comments/route.ts
+++ app/api/posts/[id]/comments/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: NextRequest, { params }: {
 }) {
     const res = await fetch('https://jsonplaceholder.typicode.com/comments/');
     const data = await res.json();
-    return new NextResponse(JSON.stringify(data.filter(x => {
+    return new NextResponse(JSON.stringify(data.filter(function(x: any) {
         return Number(x.postId) === Number(params.id)
     })));
 
